2354e914bcb77edefbf4820a357f796700087ef1
Sebastian Herber <herber@zaplab.at>
2015-02-17 20:55:21 +0100

    Adds time-grunt package
    to see how long each task took

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,8 @@
 module.exports = function(grunt) {
     'use strict';
 
+    require('time-grunt')(grunt);
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         banner: '/*!\n' +
