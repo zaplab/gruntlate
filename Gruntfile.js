8976d25ae7cd75404e3f829af0430304a55f8c2d
Sebastian Herber <herber@zaplab.at>
2015-05-09 15:19:05 +0200

    [FEATURE] Adds load-grunt-tasks module

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function(grunt) {
     'use strict';
 
     require('time-grunt')(grunt);
+    require('load-grunt-tasks')(grunt);
 
     var isDevMode,
         target = grunt.option('target');
@@ -189,19 +190,6 @@ module.exports = function(grunt) {
             }
         }
     });
-
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-csslint');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-header');
-    grunt.loadNpmTasks('grunt-mocha');
-    grunt.loadNpmTasks('grunt-sass');
 
     // Testing
     grunt.registerTask('test-css', [
